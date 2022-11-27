cac391c93832eee6163f97d9c5f87b3741c1d5c4
h-buzzi <h-buzzi>
2022-11-27 10:44:20 -0300

    Added input from query parameters

--- functions/src/http.ts
+++ functions/src/http.ts
@@ -4,5 +4,15 @@ import * as admin from 'firebase-admin';
 admin.initializeApp();
 
 export const basicHTTP = functions.https.onRequest((request, response) => {
-    response.send('Hello from firebase');
+    var name;
+    if(request.query.name){
+        name = request.query.name;
+    } else {
+        name = request.query.Name;
+    }
+    if (name){
+        response.send(`Hello ${name} from firebase!`);
+    } else {
+        response.status(422).send(`Query parameter name is ${name}, please insert a name!`);
+    }
 })
